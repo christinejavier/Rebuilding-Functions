8010d921861304886ad7c39395b14eac54f92af4
christinejavier <cjavier13@ucsbalum.com>
2016-04-16 14:35:10 -0700

    Implements forEach within map function

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+//Option 1: doesn't use forEach
 function map(arr, callback) {
   var transformedArr = [];
   for (var i = 0; i < arr.length; i++) {
@@ -10,6 +11,29 @@ function addNum(x){
   return x + 1;
 }
 
+//Option 2: implements forEach within map
+var forEach = function(collection, callback) {
+  if (Array.isArray(collection)) {
+    for (var i = 0; i < collection.length; i++) {
+      callback(collection[i]);
+    }
+  }
+  else {
+    for (var prop in collection) {
+      callback(collection[prop]);
+    }
+  }
+}
+
+var map = function(collection, callback) {
+	var result = [];
+	forEach(collection, function(x){
+  	result.push(callback(x));
+  });
+	return result;
+}
+
+//////////////////////////////////////
 var originalArr = [1, 2, 3, 3, 4, 5];
 
 console.log(
@@ -25,5 +49,5 @@ console.log(
   originalArr
   //returns [1, 2, 3, 4, 5]
   //originalArr remains unchanged.
-  //use map to return a transformed array independent of original input array  
+  //use map to return a transformed array independent of original input array
 );
